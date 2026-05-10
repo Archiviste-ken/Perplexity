614f172d8717bbd53c167afae61dc05699ac6f77
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-10 15:52:03 +0530

    feat: define user schema with username and password fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,16 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    
-
-})
+  username: {
+    type: String,
+    unique: [true, "the username should be unique"],
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: [true, "the password should be unique"],
+    required: true,
+    select: false
+  }
+  
+});
