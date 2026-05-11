654d214f5a67cd47ebe748445757d5168a435510
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-11 16:20:11 +0530

    feat: implement user model with password hashing and comparison methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,16 +1,47 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: [true, "the username should be unique"],
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: [true, "the username should be unique"],
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: [true, "the emaill should be unique"],
+    },
+    password: {
+      type: String,
+      unique: [true, "the pasword should be unique"],
+      required: true,
+      select: false,
+      minlength: 6,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    unique: [true, "the password should be unique"],
-    required: true,
-    select: false
-  }
-  
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
