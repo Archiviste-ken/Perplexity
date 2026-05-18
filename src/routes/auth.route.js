2f11fd1a6bc6a4203bea9b66c96ca361605ddd80
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-18 18:50:31 +0530

    feat: add login functionality with validation to authentication routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,15 @@
 import Router from "express";
 import { verifyEmail,registerUser } from "../controllers/auth.controllers.js";
-import { registerValidator } from "../validators/auth.validator.js";
+import { loginValidator, registerValidator } from "../validators/auth.validator.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", registerValidator, registerUser);
 
 authRouter.post("/verifyEmail",verifyEmail)
+
+
+authRouter.post("/login",loginValidator, loginUser)
 
 
 
